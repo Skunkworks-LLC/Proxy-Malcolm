f5489341e061f0bba75bb8b0f8e695d905e3f1cb
Mjones13 <jonesmal@umich.edu>
2018-09-22 14:25:29 -0700

    Corrected double parentheses

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const publicFolder = path.join(__dirname, '../public/');
 app.use(express.static(publicFolder));
 app.use('/projects/:id', express.static(publicFolder));
 
-app.all(('/projects/:id'), (req, res) => {
+app.all('/projects/:id', (req, res) => {
   var projectId = req.url.slice(9);
   res.redirect(307, `${projectId}`);
-})
+});
 
 app.listen(2424, () => {
   console.log('Server is connected @ port 2424');
